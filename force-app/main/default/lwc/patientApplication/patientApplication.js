121a9be8e1e385eebe0fe2e7a5b436cb40d78377
JoshCruz <josh@digitalmonestary.com>
2021-07-22 12:42:01 -0700

    added comments for clarity

--- force-app/main/default/lwc/patientApplication/patientApplication.js
+++ force-app/main/default/lwc/patientApplication/patientApplication.js
@@ -1,3 +1,8 @@
+/* @author Josh Cruz
+* @date 07/21/21
+* @description Display logic for Patient Application LWC
+*/
+
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent'
 import createApplication from "@salesforce/apex/PatientApplicationController.updatePatientApplication";
@@ -5,10 +10,13 @@ import createApplication from "@salesforce/apex/PatientApplicationController.upd
 export default class PatientApplication extends LightningElement {
   applicantId;
 
+  // on a success record creation we grab the recordID 
+  //and assign it to a program specialist to match to a program
   handleSuccess(event) {
     const payload = event.detail;
     console.log(JSON.stringify(payload));
     this.applicantId = event.detail.id;
+    //fires off to apex controller
     createApplication({ applicantId: this.applicantId })
       .then((data) => {
         console.log(JSON.stringify(data));
@@ -29,13 +37,15 @@ export default class PatientApplication extends LightningElement {
     this.dispatchEvent(event);
   }
 
+  // allows for us to manipulate data before database
   handleSubmit(event) {
     event.preventDefault();       // stop the form from submitting
     const fields = event.detail.fields;
     // console.log("🚀 ~ file: PatientApplication.js ~ line 21 ~ PatientApplication ~ handleSubmit ~ fields", fields)
-
     this.template.querySelector('lightning-record-edit-form').submit(fields);
   }
+
+  //fires off an error for the user to see
   handleError(event) {
     const payload = event.detail;
     console.log(JSON.stringify(payload));
